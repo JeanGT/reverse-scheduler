5ef4e3b9ecd0142538a0451b5a8dc9a9198d5391
Jean Gustavo Tomé <jean.tome@travelpassgroup.com>
2024-10-12 19:04:13 -0300

    prevent multi day event

--- assets/calendar-init.js
+++ assets/calendar-init.js
@@ -18,17 +18,35 @@ jQuery(document).ready(function ($) {
         slotMaxTime: '18:00:00',
         allDayText: 'Dia todo',
         dayHeaderFormat: { day: '2-digit', month: '2-digit' },
+        buttonText: {
+            today: 'Hoje'
+        },
         // Function to handle creating new events on select
         select: function (info) {
+            var startDate = info.start;
+            var endDate = info.end;
+
+            // If the event is spanning multiple days, prevent the event creation
+            if (startDate.getDate() !== endDate.getDate()) {
+                calendar.unselect();
+                return;
+            }
+
             calendar.addEvent({
                 start: info.start,
                 end: info.end,
                 allDay: info.allDay
             });
 
-            calendar.unselect(); 
+            calendar.unselect();
         }
     });
 
     calendar.render();
+
+    $('#submit-calendar').on('click', function () {
+        var events = calendar.getEvents();
+
+        console.log(events);
+    });
 });
